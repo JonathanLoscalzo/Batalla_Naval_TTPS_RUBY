eede33a8168522c4ee16252318964e210e1cea71
jonathanloscalzo <loscalzo.jony@gmail.com>
2015-01-18 23:26:30 -0300

    funcionan sizemodel, gamesModel y usersModel en la vista.

--- public/js/viewmodels/layout-Viewmodels.js
+++ public/js/viewmodels/layout-Viewmodels.js
@@ -1,7 +1,4 @@
   $(document).ready(function(){
-    $("#table-games").DataTable({
-      "dom": '<"top"f>rt<"bottom"p><"clear">'
-    });
     var PlayersViewModel = function(data){
       this.id = data.id;
       this.username = data.username;
@@ -18,20 +15,23 @@
       this.sizeText = data.size;
     };
 
-    var ViewModel = function(){
+    var SizeModel = function(){
       var self = this; 
-      self.UsersArray = ko.observableArray([]);
-      $.getJSON("/players", function(data){
-        var mappedPlayers = $.map(data, function(item){ return new PlayersViewModel(item)});
-        self.UsersArray(mappedPlayers);
+      self.Sizes = ko.observableArray([]);
+      $.getJSON("/sizes", function(data){
+        var mappedSizes = $.map(data, function(item){ return new SizesViewModel(item)});
+        self.Sizes(mappedSizes);
       });
+      self.selectedSize = ko.observable();
+    };
 
-      
+    var GameModel = function(){
+      var self = this;
       self.games = ko.observableArray([]);
       $.getJSON("/games", function(data){
+
         var mappedGames = $.map(data, function(item){ return new GameViewModel(item)});
         self.games = mappedGames;
-
         $.map(self.games, function(n){
           n.statusLabel = ko.pureComputed(function(){
             switch(n.status) {
@@ -45,22 +45,37 @@
               return 'label-warning';
               break;
             }
-          }, n);
-          console.log(n);
+          }, n); 
         });
+        $("#table-games").DataTable({
+          "dom": '<"top"f>rt<"bottom"p><"clear">'
+        }); 
 
       });
-
-      self.Sizes = ko.observableArray([]);
-      $.getJSON("/sizes", function(data){
-        var mappedSizes = $.map(data, function(item){ return new SizesViewModel(item)});
-        self.Sizes(mappedSizes);
-      });
-      self.selectedSize = ko.observable();
     };
 
-    var elem = new ViewModel();
+    var UsersModel = function(){
+      var self = this;
+      self.UsersArray = ko.observableArray([]);
+      $.getJSON("/players", function(data){
+        var mappedPlayers = $.map(data, function(item){ return new PlayersViewModel(item)});
+        self.UsersArray(mappedPlayers);
+      });
+    }
 
 
-    ko.applyBindings(elem);
+    var gamesModel = new GameModel();
+    var usersModel = new UsersModel();
+    var sizeModel = new SizeModel();
+    var AllModels = {
+      gamesModel : gamesModel,
+      usersModel : usersModel,
+      sizeModel : sizeModel
+    }
+    
+    /*
+      Para agregar atributos al AllModels 
+      AllModels.attr = value; => supongo.
+    */
+    ko.applyBindings(AllModels);
   });
