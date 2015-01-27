866762bb44dd300175f35e8e051dc20915b160e7
jonathanloscalzo <loscalzo.jony@gmail.com>
2015-01-26 22:16:01 -0300

    SE cambiaron unas migraciones que fallaban. Y el nuevo schema de la base de datos

--- db/migrate/20150117184053_cambiando_referencia_game_board.rb
+++ db/migrate/20150117184053_cambiando_referencia_game_board.rb
@@ -1,6 +1,6 @@
 class CambiandoReferenciaGameBoard < ActiveRecord::Migration
 	def change
-		remove_column :boards, :game_id
+		remove_column :boards, :game_id, :integer
 		add_belongs_to :games, :board1_id
 		add_belongs_to :games, :board2_id
 	end
--- db/migrate/20150117185802_agregando_boards_prueba.rb
+++ db/migrate/20150117185802_agregando_boards_prueba.rb
@@ -1,10 +1,12 @@
 class AgregandoBoardsPrueba < ActiveRecord::Migration
   def change
+=begin
   	b = Board.create(breed_id_id:1)
   	b2 = Board.create(breed_id_id:1)
   	g = Game.find(1)
   	g.board1_id_id =  b.id
   	g.board2_id_id =  b2.id
   	g.save # => no olvidar esto!!!!!
+=end
   end
 end
