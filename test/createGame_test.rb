9e1ec81db4abf56dfeaf06b0661594bc4ccf73f6
jonathanloscalzo <loscalzo.jony@gmail.com>
2015-01-28 17:45:31 -0300

    test a realizar

---
+++ test/createGame_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+# => https://github.com/brandonweiss/rack-minitest
+
+describe " when a user create a game " do
+	
+end
---
+++ test/session_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+# => https://github.com/brandonweiss/rack-minitest
+
+describe " session and registration" do
+	
+end
