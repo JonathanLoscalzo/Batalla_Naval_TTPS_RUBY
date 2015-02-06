591326cf161e82f46f5429b2492b7c6e3f0d9cd6
jonathanloscalzo <loscalzo.jony@gmail.com>
2015-02-05 21:06:01 -0300

    Se agrega simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,15 @@
 ENV['RACK_ENV'] = 'test'
 # para correr test bundle exec rake db:migrate RACK_ENV=test
 
+require 'simplecov'
+
+SimpleCov.start
+
 require 'minitest/autorun'
 require 'rack/test'
 require File.expand_path '../../app', __FILE__ # => File.expand_path '../Gemfile', File.dirname(__FILE__)
 include Rack::Test::Methods
+
 
 def app
   Application
