1fb36225afb0f895cd37467a5c3698f45c270f17
jonathanloscalzo <loscalzo.jony@gmail.com>
2015-02-07 01:00:32 -0300

    se borran test de finish(testeados en games_test)

--- test/finished_test.rb
+++ test/finished_test.rb
@@ -49,39 +49,6 @@ describe 'when a game finish' do
 		@user2.destroy
 		@game.destroy
 	end
-
-	
-
-	it 'must return finish when is the last move' do
-		#el primer jugador puede mover. el lugar es 1,3
-		login_with_user(@user1)
-		put '/games/'+@game.id.to_s+'/move', params = { 'row'=> 1,'column'=> 3 } 
-		follow_redirect!
-		last_response.body.must_include 'You win!!'
-		log_out_with_user @user1
-		login_with_user @user2
-		get '/games/'+@game.id.to_s 
-		last_response.body.must_include 'You lose'
-		log_out_with_user @user2
-
-	end
-
-	it 'wont shot a finished game' do
-		login_with_user(@user1)
-		put '/games/'+@game.id.to_s+'/move', params = { 'row'=> 1,'column'=> 3 } 
-		follow_redirect!
-		last_response.body.must_include 'You win!!'
-		put '/games/'+@game.id.to_s+'/move', params = { 'row'=> 1,'column'=> 3 } 
-		follow_redirect!
-		last_response.body.must_include "You win"
-		log_out_with_user @user1
-		login_with_user @user2
-		put '/games/'+@game.id.to_s+'/move', params = { 'row'=> 1,'column'=> 3 }
-		follow_redirect!
-		last_response.body.must_include "You lose"
-		log_out_with_user @user2
-	end
-
 	private
 	def login_with_user(user)
 		post '/login', params = { username: user.username, password:user.password }
